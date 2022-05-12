ead0a7c7cd93bd93a7927e637f5741f068629100
Douglas CH.Z <douglash.dcz@gmail.com>
2022-05-12 18:41:57 -0400

    Se aumento el campo direccion en colegio

--- Servidor/Esquemas/Colegio.js
+++ Servidor/Esquemas/Colegio.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const colegioSchema = new mongoose.Schema({
     nombreColegio: { type: String },
+    direccion: { type: String },
     telefono: { type: String}
 })
 
