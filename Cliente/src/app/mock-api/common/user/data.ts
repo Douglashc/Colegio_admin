1e4377fb916aae0451b7ecbd62f422674bc933ef
Douglas CH.Z <douglash.dcz@gmail.com>
2022-05-17 23:54:03 -0400

    Se cambio la portada

--- Cliente/src/app/mock-api/common/user/data.ts
+++ Cliente/src/app/mock-api/common/user/data.ts
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export const user = {
     id    : 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
-    name  : 'Brian Hughes',
-    email : 'hughes.brian@company.com',
-    avatar: 'assets/images/avatars/brian-hughes.jpg',
+    name  : 'Douglas Chavarria Zurita',
+    email : 'douglash.dcz@gmail.com',
+    avatar: 'assets/images/avatars/douglas.jpg',
     status: 'online'
 };
